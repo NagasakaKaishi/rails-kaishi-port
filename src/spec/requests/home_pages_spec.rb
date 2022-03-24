f6958e0c30bd67674f87664ca564d5d34591ea7e
NagasakaKaishi <kaishi0827@icloud.com>
2022-03-24 11:42:56 +0900

    ユーザー新規登録ページ作成/Userモデル作成/Usersコントローラー作成/ルーティング完了

--- src/spec/requests/home_pages_spec.rb
+++ src/spec/requests/home_pages_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "HomePages", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get root_path
+    end
+    it "レスポンスが正常である" do
+      expect(response).to have_http_status 200
+    end
   end
 end
