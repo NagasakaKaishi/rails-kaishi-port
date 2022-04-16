e2097031e0fc41ef0a625d497188459e4a07b908
NagasakaKaishi <kaishi0827@icloud.com>
2022-04-16 12:47:26 +0900

    event_paramsに:setを追加して修正

--- src/app/controllers/events_controller.rb
+++ src/app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :start_date, :end_date)
+      params.require(:event).permit(:title, :description, :set, :start_date, :end_date)
     end
 end
